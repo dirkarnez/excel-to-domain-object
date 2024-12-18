5c5f02e7873512e3dd184e81ade932b9322c46ae
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-18 18:52:05 +0800

    - update code-generator.js

--- code-generator.js
+++ code-generator.js
@@ -1,3 +1,59 @@
+function getCellValue(row, col) {
+    return `${row},${col}`
+}
+
+function generateCode(ast) {
+    switch (ast.type) {
+        case 'DIV_OP':
+            return `${generateCode(ast.left)} / ${generateCode(ast.right)}`;
+        case 'TIMES_OP':
+            return `${generateCode(ast.left)} * ${generateCode(ast.right)}`;
+        case 'PLUS_OP':
+            return `${generateCode(ast.left)} + ${generateCode(ast.right)}`;
+        case 'MINUS_OP':
+            return `${generateCode(ast.left)} - ${generateCode(ast.right)}`;
+        case 'CELL_REFERENCE':
+            return getCellValue(ast.reference.row, ast.reference.col); // Assumes a function getCellValue(row, col)
+        case 'NUMBER':
+            return ast.value.toString();
+        case 'PARENTHESES':
+            return `(${generateCode(ast.expression)})`;
+        default:
+            throw new Error(`Unknown AST node type: ${ast.type}`);
+    }
+}
+
+// Example usage
+const ast = JSON.parse(`{
+    "type": "DIV_OP",
+    "left": {
+        "type": "PARENTHESES",
+        "expression": {
+            "type": "TIMES_OP",
+            "left": {
+                "type": "CELL_REFERENCE",
+                "reference": {
+                    "col": 6,
+                    "row": 1,
+                    "type": "CELL_REFERENCE"
+                }
+            },
+            "right": {
+                "type": "NUMBER",
+                "value": 4
+            }
+        }
+    },
+    "right": {
+        "type": "NUMBER",
+        "value": 1000
+    }
+}`);
+
+const jsCode = generateCode(ast);
+console.log(jsCode);
+
+
 export default fields => `class DomainObject
 {
 ${fields
